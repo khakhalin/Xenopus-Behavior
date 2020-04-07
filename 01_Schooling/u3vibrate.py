51b3bebf3f31a4df4fb1b79c220bd94e1b2700a8
vlopez7 <57424575+vlopez7@users.noreply.github.com>
2020-04-07 10:16:02 -0400

    Complete rewrite
    The older version had a lot of unnecessary code in it. Rewrote in order to provide a simple script that tests the connection of the labjack to the computer. If all the appropriate drivers and software are locally installed, it should work.

--- 01_Schooling/u3vibrate.py
+++ 01_Schooling/u3vibrate.py
@@ -1,49 +1,21 @@
-# At this point, GoPro should be connected via wifi
-# make sure the 2.4ghz connection is selected and connect via the computer's wifi 
-from goprocam import GoProCamera, constants
-goproCamera = GoProCamera.GoPro()
-
-# Open the first found LabJack U3
-import u3
-
 import time
 
-# Tic toc function so we can keep track of the timing of stimulus presentations
-def tic():
-    #Homemade version of matlab tic and toc functions
-    import time
-    global startTime_for_tictoc
-    startTime_for_tictoc = time.time()
+import u3 # Open the first found LabJack U3
 
-def toc():
-    import time
-    if 'startTime_for_tictoc' in globals():
-        print "Elapsed time is " + str(time.time() - startTime_for_tictoc) + " seconds."
-    else:
-        print "Toc: start time not set"
+vibrator = u3.U3() # defines vibrator variable as u3 labjack
 
+def stimulate():
+    FIO4_STATE_REGISTER = 6004 # defines which connection will be used to deliver power to vibrator
+    vibrator.writeRegister(FIO4_STATE_REGISTER, 1) # Delivers power to FIO4 to power the vibrator
+    time.sleep(0.15) # wait for 0.15 seconds, the length of the stimulus vibration
+    vibrator.writeRegister(FIO4_STATE_REGISTER, 0) # Delivers power to FIO4 to power the vibrator
+    
+def start(): # defines a function to test labjack-power_relay-computer connection
+    for x in range(4): # will activate the setup to trigger a series of 4 vibrations that are each two seconds apart 
+        stimulate()
 
-vibrator = u3.U3()
+        time.sleep(2)
 
-FIO4_STATE_REGISTER = 6004
-tic()
-stim_stamp = [] # creates empty vector to store time stamps of stimulus presentations
+start()
 
-for x in range(1,6):
-# Set pin assignments to the factory default condition
-    vibrator.writeRegister(FIO4_STATE_REGISTER, 0) # Set FIO4 low, LED on
-
-# Deliver voltage to FIO4 to power the vibrator
-
-    vibrator.writeRegister(FIO4_STATE_REGISTER, 1) # Set FIO4 high, LED off
-
-    time.sleep(0.15) # wait for 0.15 seconds
-    toc()
-    stim_stamp.append(str(time.time() - startTime_for_tictoc))
-    vibrator.writeRegister(FIO4_STATE_REGISTER, 0)  # Set FIO4 low, LED on
-
-    time.sleep(30) # wait ten seconds
-
-    print x
-
-    print stim_stamp
+vibrator.close()
