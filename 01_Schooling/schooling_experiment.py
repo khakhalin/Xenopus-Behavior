1af7a622aa5433d621f55aecdc14c72184fd42c2
vlopez7 <57424575+vlopez7@users.noreply.github.com>
2020-04-07 11:01:08 -0400

    Updated
    Changed runtime to 4000 seconds because 4200 was giving 14 instead of 12 images.
    
    Also removed 'import u3' and 'vibrator = u3.U3()' statements from stimulate function because it was throwing an error for use of the labjack. 
    
    Ran it through in conjunction with goprostream.py and works perfect.

--- 01_Schooling/schooling_experiment.py
+++ 01_Schooling/schooling_experiment.py
@@ -6,28 +6,28 @@ from goprocam import GoProCamera, constants # imports GoPro capabilities
 gpCam = GoProCamera.GoPro() # defines gpCam as variable
 
 import time
+import u3 # Open the first found LabJack U3
+vibrator = u3.U3() # defines vibrator variable as u3 labjack
 
 def snapshot():
     gpCam.take_photo() # initializes GoPro photo acquisition
     p = time_stamp() # creates variable to add to the list of photo time stamps
     photo_stamp.append(p) # adds the time stamp variable from line above to the list
-    print (photo_stamp) # prints the list
+    print ("Photo time stamps: ", photo_stamp) # prints the list
 
 def stimulate():
-    import u3 # Open the first found LabJack U3
-    vibrator = u3.U3() # defines vibrator variable as u3 labjack
     FIO4_STATE_REGISTER = 6004 # defines which connection will be used to deliver power to vibrator
     vibrator.writeRegister(FIO4_STATE_REGISTER, 1) # Deliver voltage to FIO4 to power the vibrator
     time.sleep(0.15) # wait for 0.15 seconds, the length of the stimulus vibration
     t = time_stamp() # creates variable to add to the list of stimulus time stamps
     stim_stamp.append(t) # adds the time stamp variable from the line above to the list
-    print (stim_stamp) # prints the list
+    print ("Stimuli time stamps: ", stim_stamp) # prints the list
     vibrator.writeRegister(FIO4_STATE_REGISTER, 0) # Deliver voltage to FIO4 to power the vibrator
 
 global start_timer # makes this a global variable
 start_timer = time.time() # creates variable for experiment start time
-end_timer = start_timer + 4200 # this is when the experiment should stop running, should be enough time to acquire 12 images
-print (start_timer)
+end_timer = start_timer + 4000 # this is when the experiment should stop running, should be enough time to acquire 12 images
+print ("Experiment start time: ", start_timer)
 
 
 def time_stamp(): # this function is used to get time stamps for stimuli presentations & photos
